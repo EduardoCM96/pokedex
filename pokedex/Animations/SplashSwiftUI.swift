814ca48704d06cbc6ab9e30035690ca69d96a635
Eduardo Carranza <88063305+EduardoCM96@users.noreply.github.com>
2024-09-03 14:36:26 -0500

    add animation

---
+++ pokedex/Animations/SplashSwiftUI.swift
@@ -0,0 +1,43 @@
+//
+//  SplashSwiftUI.swift
+//  pokedex
+//
+//  Created by Eduardo Carranza maqueda on 03/09/24.
+//
+
+import SwiftUI
+import Lottie
+
+struct SplashSwiftUI: View {
+    var img: String = "pokeball.json"
+    var didFinish: (() -> Void)? = nil
+    var body: some View {
+        
+        VStack{
+            HStack(content: {
+                Image(.pokemon)
+                           .resizable()
+                           .scaledToFit()
+                           .aspectRatio(contentMode: .fit)
+                           .padding()
+                    
+            })
+            
+            LottieView(animation: .named(img))
+                .configure { LottieAnimationView in
+                    LottieAnimationView.contentMode = .scaleAspectFit
+                }
+            
+                .playbackMode(.playing(.toProgress(1, loopMode: .playOnce)))
+                .animationSpeed(1.5)
+                .animationDidFinish { completed in
+                    didFinish?()
+                }
+        }
+        
+    }
+}
+
+#Preview {
+    SplashSwiftUI()
+}
--- pokedex/ContentView.swift
+++ pokedex/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isActive: Bool = true
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            if isActive {
+                SplashSwiftUI()
+            } else{
+                Pokedex()
+            }
         }
-        .padding()
+        .onAppear(perform: {
+            withAnimation(.easeInOut(duration: 5).delay(2)) {
+                isActive.toggle()
+            }
+        })
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Pokedex: View {
+    var body: some View {
+        Text("Pokedex")
+    }
 }
